b04507d443f037670f1c0e0db541c3c22bb530ce
vandana <vandana123annusah@gmail.com>
2022-02-25 13:14:06 +0530

    add

--- dist/Node_task.e31bb0bc.js
+++ dist/Node_task.e31bb0bc.js
@@ -25062,7 +25062,9 @@ console.log(now.add(7, "y"));
 console.log(now.subtract(7, 'd'));
 console.log(now.subtract(7, 'M'));
 console.log(now.subtract(7, 'y'));
-console.log(moment('2020-01-01').isAfter('2018-01-01'));
+console.log(now.diff("2014-11-11" && "2015-09-11"));
+console.log(now.diff("2014-11-27" && "2015-09-16"));
+console.log(moment("2020-01-01").isAfter("2018-01-01"));
 console.log(moment('2010-01-01').isAfter('2018-01-01'));
 console.log(moment([2019]).isLeapYear());
 console.log(moment([2020]).isLeapYear());
@@ -25094,7 +25096,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43129" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36829" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -16,9 +16,12 @@ console.log(now.subtract(7, 'd'));
 console.log(now.subtract(7, 'M'));
 console.log(now.subtract(7, 'y')); 
 
-console.log(moment('2020-01-01').isAfter('2018-01-01'));
-console.log(moment('2010-01-01').isAfter('2018-01-01'));
+console.log(now.diff("2014-11-11" && "2015-09-11"));
+console.log(now.diff("2014-11-27" && "2015-09-16"));
 
+
+console.log(moment("2020-01-01").isAfter("2018-01-01"));
+console.log(moment('2010-01-01').isAfter('2018-01-01'));
 console.log(moment([2019]).isLeapYear());
 console.log(moment([2020]).isLeapYear());
 
